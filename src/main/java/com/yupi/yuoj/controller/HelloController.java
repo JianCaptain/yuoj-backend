287affe08f0c60938ec33a832c5b4b7ce5165fc7
zhangjian <631660897@qq.com>
2025-07-20 21:10:31 +0800

    增加健康检查接口

--- src/main/java/com/yupi/yuoj/controller/HelloController.java
+++ src/main/java/com/yupi/yuoj/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.yupi.yuoj.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +13,8 @@ public class HelloController {
         return "Hello " + name;
     }
 
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
 }
