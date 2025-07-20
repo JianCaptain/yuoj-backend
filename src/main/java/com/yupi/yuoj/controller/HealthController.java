d29bdf76be142b51373c77042f6aa1afea8ecee8
zhangjian <631660897@qq.com>
2025-07-20 21:14:59 +0800

    修改健康检查类名

--- src/main/java/com/yupi/yuoj/controller/HealthController.java
+++ src/main/java/com/yupi/yuoj/controller/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //标识当前类是一个请求处理类
-public class HelloController {
+public class HealthController {
 
     @RequestMapping("/hello") //标识请求路径
     public String hello(String name){
